ee28268a7b8f3ac6bb9fde9d57ee81169e4f9930
Firose Hussain <github@firumon.com>
2018-11-19 00:32:46 +0530

    Appframe model made use

--- src/Model/ProductImage.php
+++ src/Model/ProductImage.php
@@ -2,9 +2,15 @@
 
 namespace Milestone\Teebpd\Model;
 
-use Illuminate\Database\Eloquent\Model;
+//use Illuminate\Database\Eloquent\Model;
+use Milestone\Appframe\Model\Model;
 
 class ProductImage extends Model
 {
+    public $storage = [
+        ['disk' => 'tpi', 'path' => ''],
+    ];
+
+    public $files = ['image'];
     protected $table = 'product_images';
 }
