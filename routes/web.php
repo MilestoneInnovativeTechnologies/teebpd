99775f22e19e7ff8e1015f6d6ce0ce9338f241a0
Firose Hussain <github@firumon.com>
2018-11-19 00:28:45 +0530

    Namespace added for Teebs Controller

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,19 @@
 <?php
 
-Route::view('/','Teebpd::index')->name('index');
+Route::group([
+    'namespace' => 'Milestone\\Teebpd\\Controller'
+],function(){
+    Route::view('/', 'teebpd::products_index')->name('home');
+    Route::get('product/{id}', 'ProductController@detail')->name('product.detail');
+    Route::get('wishlist', 'WishListController@page')->name('wishlist');
+    Route::get('wishlist/{id}', 'WishListController@detail')->name('wishlist.detail');
+    Route::post('visitor_store', 'VisitorController@store')->name('store.visitor');
+    Route::post('visitor_clear', 'VisitorController@clear')->name('clear.visitor');
+    Route::post('wishlist', 'WishListController@store');
+    Route::post('wishlist_addproduct', 'WishListController@product')->name('wishlist.add.product');
+    Route::post('wishlist_note', 'WishListController@note')->name('add.note');
+    Route::post('wishlist_share', 'WishListController@share')->name('share');
+    Route::get('wishlist_share_alter', 'WishListController@alter')->name('share.alter');
+    Route::post('wishlist_product_alter', 'WishListController@edit')->name('product.alter');
+    Route::post('wishlistproduct_note', 'WishListProductController@note')->name('wlp.note');
+});
