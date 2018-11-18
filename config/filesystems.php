1165d76257a5d8d5a8dc4b397b6e98023ec8d600
Firose Hussain <github@firumon.com>
2018-11-19 00:27:30 +0530

    Config file for filesystem disk, storing product images

---
+++ config/filesystems.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'tpi' => [
+        'driver' => 'local',
+        'root' => public_path('images/product'),
+        'url' => env('APP_URL').'/images/product',
+        'visibility' => 'public',
+    ]
+]
+?>
