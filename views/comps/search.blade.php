f4e96d70bca33ee8b688a203301882392be7aa23
Firose Hussain <github@firumon.com>
2018-11-21 22:28:55 +0530

    Form action added to make search result display in home page

--- views/comps/search.blade.php
+++ views/comps/search.blade.php
@@ -2,7 +2,7 @@
 @push('extra')
     <div class="search-overlay"><span class="close-search"><i class="icon_close"></i></span>
         <div class="container wrapper-search">
-            <form role="search" method="get" class="searchform search-from ajax-search"><input
+            <form role="search" method="get" action="{{ route('home') }}" class="searchform search-from ajax-search"><input
                     type="text" value="" name="s" class="input-search s" placeholder="Search" autocomplete="off">
                 <button class="searchsubmit btn" type="submit"><i class="icon_search"></i></button>
             </form>
