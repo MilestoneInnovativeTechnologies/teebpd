054d6c6cb7826dc9c7ced37e23f2fefcdd9b65c1
Firose Hussain <github@firumon.com>
2018-11-30 01:34:13 +0530

    Events dispatches for email sending

--- src/Model/VisitorWishlist.php
+++ src/Model/VisitorWishlist.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class VisitorWishlist extends Model
 {
     protected $table = 'visitor_wishlists';
+
+    protected $dispatchesEvents = [
+        'created' => \Milestone\Teebpd\Event\NewWishlistShare::class
+    ];
+
+    public function Wishlist(){
+        return $this->belongsTo(Wishlist::class,'wishlist');
+    }
+
+    public function Visitor(){
+        return $this->belongsTo(Visitor::class,'visitor');
+    }
 }
