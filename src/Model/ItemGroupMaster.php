8ed428e8e6745dbac5e1785018cca744c4907b71
Firose Hussain <github@firumon.com>
2018-11-30 01:30:32 +0530

    Category and Brand moved to item group

---
+++ src/Model/ItemGroupMaster.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Milestone\Teebpd\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ItemGroupMaster extends Model
+{
+    protected $table = 'item_group_master';
+
+    public function scopeCategory($Q){
+        return $Q->where('catecode','1');
+    }
+
+    public function scopeBrand($Q){
+        return $Q->where('catecode','2');
+    }
+
+    public function scopeSize($Q){
+        return $Q->where('catecode','3');
+    }
+
+    public function scopeColor($Q){
+        return $Q->where('catecode','4');
+    }
+	
+	public function CategoryProducts(){
+		return $this->hasMany(Product::class,'category_01');
+	}
+
+	public function BrandProducts(){
+		return $this->hasMany(Product::class,'category_02');
+	}
+
+	public function SizeProducts(){
+		return $this->hasMany(Product::class,'category_03');
+	}
+
+	public function ColorProducts(){
+		return $this->hasMany(Product::class,'category_04');
+	}
+}
